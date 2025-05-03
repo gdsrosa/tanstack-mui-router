e8e73deb8cf95172355da89a46a4cb7f6f148765
Gabriel Rosa <gabrieldsrosa@gmail.com>
2025-05-03 08:44:59 +0100

    feat(app): add to access the debts from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,19 @@
+import { SLink } from "@/components/Link"
 import { Grid, Typography } from "@mui/material"
 
 function Home() {
   return (
-    <Grid container>
+    <Grid container flexDirection="column" paddingLeft={2}>
       <Typography
         component="h1"
         fontWeight="bold"
-        paddingLeft={2}
         color="primary.main"
         variant="h4"
         sx={{ marginBottom: 2 }}
       >
         Home
       </Typography>
+      <SLink to="/debts">Check my current Debts 🏦</SLink>
     </Grid>
   )
 }
